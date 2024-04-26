8f94acdba730b7d8e38a01b7948888b8ea668dfa
roopxx <127219908+roopxx@users.noreply.github.com>
2024-04-26 10:57:32 +0530

    Complete final challenge

--- models/author.js
+++ models/author.js
@@ -38,5 +38,13 @@ AuthorSchema.virtual("lifespan").get(function () {
   } - ${this.date_of_death ? DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED) : ""}`;
 });
 
+AuthorSchema.virtual("date_of_birth_yyyy_mm_dd").get(function () {
+  return DateTime.fromJSDate(this.date_of_birth).toISODate(); // format 'YYYY-MM-DD'
+});
+
+AuthorSchema.virtual("date_of_death_yyyy_mm_dd").get(function () {
+  return DateTime.fromJSDate(this.date_of_death).toISODate(); // format 'YYYY-MM-DD'
+});
+
 // Export model
 module.exports = mongoose.model("Author", AuthorSchema);
